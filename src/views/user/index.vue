f020ef97dd53c5ed73a6b778627f2a767fdb4181
zhangshubo <zhang.shubo@vision-iot.co>
2021-06-17 10:36:27 +0800

    基础配置

---
+++ src/views/user/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="user">
+    <el-input :placeholder="input.placeholder" v-model="input.input" class="input-with-select" >
+      <el-button slot="append" icon="el-icon-search"></el-button>
+    </el-input>
+    <el-input type="textarea" :rows="2" :placeholder="text.placeholder" v-model="text.textarea" ></el-input>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "user",
+  components: {
+  },
+  data() {
+    return {
+      input: {
+        placeholder:"请输入查询内容",
+        input:"",
+      },
+      text:{
+        placeholder:"内容显示",
+        textarea:"",
+      }
+    };
+  },
+};
+</script>
+<style lang="scss">
+.el-textarea{
+  margin-top: 20px;
+}
+</style>
